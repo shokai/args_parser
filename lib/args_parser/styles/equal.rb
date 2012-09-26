9a88316d89ba1bc662a6cf19eaf42080102186e7
Sho Hashimoto <hashimoto@shokai.org>
2012-09-26 15:07:30 +0900

    fix equal style regex

--- lib/args_parser/styles/equal.rb
+++ lib/args_parser/styles/equal.rb
@@ -4,15 +4,15 @@ module ArgsParser
 
     def parse_style_equal(argv)
       argv.each_with_index do |arg, i|
-        if arg =~ /^--+[^-=]+$/
-          k,v = [arg.scan(/^--+([^-=]+)$/)[0][0], true]
-        elsif arg =~ /^--+[^-=]+=[^=]+$/
-          k,v = arg.scan(/^--+([^-=]+)=([^=]+)$/)[0]
+        if arg =~ /^--+[^-=\s]+$/
+          k,v = [arg.scan(/^--+([^-=\s]+)$/)[0][0], true]
+        elsif arg =~ /^--+[^-=\s]+=[^=\s]+$/
+          k,v = arg.scan(/^--+([^-=\s]+)=([^=\s]+)$/)[0]
         elsif i == 0
           @first = arg
         end
         if k and v
-          k = k.to_sym
+          k = k.strip.to_sym
           k = aliases[k] if aliases[k]
           params[k][:value] = v
         end
