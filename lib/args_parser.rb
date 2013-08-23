6961362a9b9651d2baedfe4d3c585a500185d916
Sho Hashimoto <hashimoto@shokai.org>
2013-08-23 19:34:37 +0900

    move module method into args_parser/main.rb

--- lib/args_parser.rb
+++ lib/args_parser.rb
@@ -1,6 +1,4 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
+require 'args_parser/main'
 require 'args_parser/parser'
 require 'args_parser/styles/default'
 require 'args_parser/styles/equal'
--- lib/args_parser/parser.rb
+++ lib/args_parser/parser.rb
@@ -1,12 +1,4 @@
 module ArgsParser
-  def self.parse(argv=[], config={}, &block)
-    Config::DEFAULT.each do |k,v|
-      config[k] = v unless config[k]
-    end
-    parser = Parser.new(config, &block)
-    parser.parse argv
-    parser
-  end
 
   class Parser
     attr_reader :argv, :params, :aliases
